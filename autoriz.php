7ae7f9b5b52fc6d85a699601076e8d10df5baec7
mkoyashi <masaiikoyashi@gmail.com>
2021-06-08 00:14:32 +0300

    йц

---
+++ autoriz.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и htubcnhfwbz</title>
+    <link rel ="stylesheet" href="assets/css/main.css"
+</head>
+<body>
+    <form action = "" method = "">
+    <label>Логин</label>
+    <input type = "text" placeholder ='Введите логин'>
+    <label>Пароль</label>
+    <input type = "password" placeholder ='Введите пароль'>
+    <button>Войти</button>
+    <p>Если у вас нет аккаунта - <A href = "register.php">зарегистрируйтесь</a>!</p>
+</form>
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Авторизация и htubcnhfwbz</title>
+    <link rel ="stylesheet" href="assets/css/main.css"
+</head>
+<body>
+    <form action = "" method = "">
+    <label>ФИО</label>
+    <input type = "text" placeholder ='Введите свое ФИО'>
+    <label>Логин</label>
+    <input type = "text" placeholder ='Введите логин'>
+    <label>Почта</label>
+    <input type = "email" placeholder ='Введите адрес вашей электронной почты'>
+    <label>Пароль</label>
+    <input type = "password" placeholder ='Введите пароль'>
+    <label>Подтверждение пароля</label>
+    <input type = "password" placeholder ='Подтвердите пароль'>
+    <button>Зарегистрироваться</button>
+    <p>Если у вас уже есть аккаунт - <A href = "/autoriz.php">авторизируйтесь</a>!</p>
+</form>
+</body>
+</html>
